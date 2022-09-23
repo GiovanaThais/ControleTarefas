e5325f9e03a167b0eb9654c6211a92de68a0c786
giovana.thais <giovana.thais@ecwsa.com.br>
2022-09-22 22:50:51 -0300

    test: testes do modulo unittest

--- tarefas/tests.py
+++ tarefas/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
 
 # Create your tests here.
+#No Django, o melhor método de escrever os testes é através do uso do módulo unittest do Python
---
+++ tarefas/tests/test_models.py
@@ -0,0 +1,13 @@
+# from django.test import TestCase
+# from django.template.defaultfilters import slugify
+# # from blogsite.models import Post
+
+
+# class ModelsTestCase(TestCase):
+#     def test_post_has_slug(self):
+#         """Posts are given slugs correctly when saving"""
+#         post = Post.objects.create(title="My first post")
+
+#         post.author = "Giovana"
+#         post.save()
+#         self.assertEqual(post.slug, slugify(post.title))
